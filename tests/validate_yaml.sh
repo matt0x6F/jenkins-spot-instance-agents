cb11fd7e5b9694580f7eb4653f2515d9f3c5e305
Matt Ouille <matthew.ouille@gmail.com>
2017-10-10 23:59:34 -0500

    Added YAML validation test

---
+++ tests/validate_yaml.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+declare -i RESULT=0
+set +e # Don't exit on error. Collect the errors instead.
+YAML_PATH_LIST=`ls | grep -F '.yaml'`
+echo 'YAML files to check syntax:'; echo "$YAML_PATH_LIST"; echo "";
+for YAML_PATH in $YAML_PATH_LIST; do
+   ruby -e "require 'yaml'; YAML.load_file('${YAML_PATH}')"
+   RESULT+=$?
+done
+exit $RESULT
